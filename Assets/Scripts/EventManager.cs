557dedfca81b97b9fe9c0ddca3a6f79222483501
Napalm338 <73107063+Napalm338@users.noreply.github.com>
2021-12-30 18:12:24 +0100

    Aggiunto danni da Asteroide

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+    public delegate void StartGameDelegate();
+    public static StartGameDelegate onStartGame;
+
+    public delegate void TakeDamageDelegate(float amt);
+    public static TakeDamageDelegate onTakeDamage;
+
+    public static void StartGame(){
+      if(onStartGame != null){
+        onStartGame();
+      }
+    }
+
+    public static void TakeDamage(float percent)
+    {
+      Debug.Log("Take Damage: " + percent);
+
+      if(onTakeDamage != null)
+          onTakeDamage(percent);
+    }
+}
